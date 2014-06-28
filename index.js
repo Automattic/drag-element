88c06e7ef62465ce2983436a9103e47107804ab8
Marco Aurelio <thecoreh@gmail.com>
2014-06-27 22:48:50 -0300

    Use closest range instead of exact range.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var atxy = require('range-at-xy');
+var ctoxy = require('range-closest-to-xy');
 var split = require('split-at-range');
 var Emitter = require('emitter');
 var debug = require('debug')('drag');
@@ -49,7 +49,7 @@ Drag.prototype.update = function(el, x, y) {
     }
     if (el == this.source.element) return;
     this.destination.element = el;
-    var range = this.destination.range = atxy(el, x, y);
+    var range = this.destination.range = ctoxy(el, x, y);
     var rect = el.getBoundingClientRect();
     if (range) {
       if (y <= rect.top + DROP_PADDING) {
